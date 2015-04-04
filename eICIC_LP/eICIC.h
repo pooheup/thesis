fa6ed3b718bf1db67fca91b9cf7fbf2329fef3b6
leafriend <leafriend@gmail.com>
2015-04-04 20:17:06 +0900

    사용하지 않는 함수 삭제
    exhaustive 함수

--- eICIC_LP/eICIC.h
+++ eICIC_LP/eICIC.h
@@ -14,10 +14,6 @@ double cal_thrpt_s(double _sinr, double _BW);
 
 double cal_thrpt_i(double _channel_gain, double _interference, double _no );
 
-void exhaustive_search_calculation(int _user_num, double _objective_value, double *_best_value, int *_state_temp, int *_state_best, Mobile **mobiles, Macro **macros, double *_lambda, double *_thrpt_macro, double *_thrpt_ABS, double *_trhpt_nonABS);
-
-void exhaustive_search_call_next_user(int _user_num, double _objective_value, double *_best_value, int *_state_temp, int *_state_best, Mobile **mobiles, Macro **macros, double *_lambda, double *_thrpt_macro, double *_thrpt_ABS, double *_trhpt_nonABS);
-
 void PA1_calculation(int _macro_num, double *_best_value, int *_state_temp, int *_state_best, int *_user_state_best, Mobile **mobiles, Pico **picos, Macro **macros, double *_lambda, double *_thrpt_macro, double *_thrpt_ABS, double *_thrpt_nonABS);
 
 void PA1_call_next_pico(int _macro_num, double *_best_value, int *_state_temp, int *_state_best, int *_user_state_best, Mobile **mobiles, Pico **picos, Macro **macros, double *_lambda, double *_thrpt_macro, double *_thrpt_ABS, double *_thrpt_nonABS);
