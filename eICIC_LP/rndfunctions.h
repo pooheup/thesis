83145563486f94eda89aaa522ec4022466a7ab85
leafriend <leafriend@gmail.com>
2015-04-06 04:45:20 +0900

    매크로를 매크로 함수로 다시 변경

--- eICIC_LP/rndfunctions.h
+++ eICIC_LP/rndfunctions.h
@@ -4,8 +4,8 @@
 double uniform(void);
 double gaussian(double mu, double sigma);
 
-#define RAYLEIGH    sqrt(pow(gaussian(0, 1/1.2533), 2) + pow(gaussian(0, 1/1.2533), 2))
+#define RAYLEIGH()      sqrt(pow(gaussian(0, 1/1.2533), 2) + pow(gaussian(0, 1/1.2533), 2))
 
-#define LOG_NORMAL  pow(10, gaussian(0, LN_SHAD) * 0.1)
+#define LOG_NORMAL()    pow(10, gaussian(0, LN_SHAD) * 0.1)
 
 #endif /*_RNDFUNCTIONS_H_*/
